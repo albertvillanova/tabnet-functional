02b7916a0afd3d2bdbb833758763b2f87727abb4
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-11-06 15:42:32 +0100

    Add outputs from Feature Transformer

--- tabnet/model.py
+++ tabnet/model.py
@@ -79,4 +79,15 @@ for ni in range(NUM_DECISION_STEPS):
 
     #
     if ni > 0:
-        pass
+        decision_out = tf.nn.relu(x[:, :OUTPUT_DIM])
+        # Decision aggregation
+        output_aggregated += decision_out
+        # Aggregated masks are used for visualization of the
+        # feature importance attributes
+        scale_agg = tf.reduce_sum(
+            decision_out, axis=1, keep_dims=True) / (
+                            NUM_DECISION_STEPS - 1)
+        aggregated_mask_values += mask_values * scale_agg
+
+    #
+    features_for_coef = (x[:, OUTPUT_DIM:])
