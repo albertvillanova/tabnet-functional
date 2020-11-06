7615e45b5f430b2472d619a6ec16ace68e1c1ed5
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-11-06 23:07:29 +0100

    Add train

---
+++ tabnet/train.py
@@ -0,0 +1,26 @@
+# import tensorflow as tf
+from tensorflow import keras
+
+from .model import classifier as model
+
+# TODO: refactorize as config
+DECAY_RATE = 0.95
+DECAY_STEPS = 500  # DECAY_EVERY
+INITIAL_LEARNING_RATE = 0.02  # INIT_LEARNING_RATE
+
+
+# TODO: model returns not only logits but (logits, total_entropy)
+
+# TODO: customize loss with total_entropy
+loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+learning_rate = keras.optimizers.schedules.ExponentialDecay(
+    INITIAL_LEARNING_RATE, DECAY_STEPS, DECAY_RATE
+)
+optimizer = keras.optimizer.AdamOptimizer(learning_rate=learning_rate)
+
+model.compile(
+    loss=loss,
+    optimizer=optimizer,
+    metrics=["accuracy"],
+)
